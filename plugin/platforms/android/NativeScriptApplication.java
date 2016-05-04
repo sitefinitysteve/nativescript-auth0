936b79504833d51c7b73da1d4cf3188354aab886
Luciano Balmaceda <balmacedaluciano@gmail.com>
2016-05-04 15:28:20 -0300

    update plugin Application and Activity classes

--- plugin/platforms/android/NativeScriptApplication.java
+++ plugin/platforms/android/NativeScriptApplication.java
@@ -1,56 +1,72 @@
 package com.tns;
 
-import android.app.Application;
+@com.tns.JavaScriptImplementation(javaScriptFile = "./tns_modules/application/application.js")
+public class NativeScriptApplication extends android.app.Application implements com.tns.NativeScriptHashCodeProvider,
+ com.auth0.lock.LockProvider {  // AUTH0 INTERFACE
+	private static android.app.Application thiz;
+	// AUTH0 START
+  private com.auth0.lock.Lock lock;
+	// AUTH0 END
 
-@JavaScriptImplementation(javaScriptFile = "app/tns_modules/application/application.js")
-public class NativeScriptApplication extends android.app.Application implements com.tns.NativeScriptHashCodeProvider, com.auth0.lock.LockProvider {
+	public NativeScriptApplication(){
+		super();
+		thiz = this;
+	}
 
-    private static NativeScriptApplication thiz;
-    private com.auth0.lock.Lock lock;
+	// AUTH0 START
+	public com.auth0.lock.Lock getLock() {
+		System.out.println("AUTH0DEBUG: Getting Lock instance");
+    return lock;
+  }
+	// AUTH0 END
 
-    public NativeScriptApplication()
-    {
-        thiz = this;
-    }
-    
-    public com.auth0.lock.Lock getLock() {
-        return lock;
-    }
-
-    public void onCreate() {
+	public void onCreate()  {
 		new RuntimeHelper(this).initRuntime();
-        Platform.initInstance(this);
-        java.lang.Object[] params = null;
-        com.tns.Platform.callJSMethod(this, "onCreate", void.class, params);
-        
-        lock = new com.auth0.lock.Lock.Builder()
+		if (!Runtime.isInitialized()) {
+			super.onCreate();
+			return;
+		}
+		java.lang.Object[] args = null;
+		com.tns.Runtime.callJSMethod(this, "onCreate", void.class, args);
+
+		// AUTH0 START
+		System.out.println("AUTH0DEBUG: Initializing Lock instance");
+		lock = new com.auth0.lock.Lock.Builder()
         .loadFromApplication(this)
         /** Other configuration goes here */
         .closable(true)
         .build();
-    }
+		// AUTH0 END
+	}
 
-    public void onLowMemory() {
-        java.lang.Object[] params = null;
-        com.tns.Platform.callJSMethod(this, "onLowMemory", void.class, params);
-    }
+	public void onLowMemory()  {
+		if (!Runtime.isInitialized()) {
+			super.onLowMemory();
+			return;
+		}
+		java.lang.Object[] args = null;
+		com.tns.Runtime.callJSMethod(this, "onLowMemory", void.class, args);
+	}
 
-    public void onTrimMemory(int level) {
-        java.lang.Object[] params = new Object[1];
-        params[0] = level;
-        com.tns.Platform.callJSMethod(this, "onTrimMemory", void.class, params);
-    }
+	public void onTrimMemory(int param_0)  {
+		if (!Runtime.isInitialized()) {
+			super.onTrimMemory(param_0);
+			return;
+		}
+		java.lang.Object[] args = new java.lang.Object[1];
+		args[0] = param_0;
+		com.tns.Runtime.callJSMethod(this, "onTrimMemory", void.class, args);
+	}
 
+	public boolean equals__super(java.lang.Object other) {
+		return super.equals(other);
+	}
 
-    public boolean equals__super(java.lang.Object other) {
-        return super.equals(other);
-    }
-    public int hashCode__super() {
-        return super.hashCode();
-    }
+	public int hashCode__super() {
+		return super.hashCode();
+	}
 
-    public static Application getInstance() {
-        return thiz;
-    }
-
+	public static android.app.Application getInstance() {
+		return thiz;
+	}
 }
