7b2d940fe1f9452defe6f65925b43b82ec5196b1
Steve McNiven-Scott <steve@sitefinitysteve.com>
2016-08-18 14:42:20 -0400

    Add newfangled custom application\activity stuff

---
+++ demo/app/activity.android.js
@@ -0,0 +1,36 @@
+var frame = require("ui/frame");
+
+var superProto = android.app.Activity.prototype;
+var Activity = android.app.Activity.extend("org.myApp.MainActivity", {
+    onCreate: function(savedInstanceState) {
+        if(!this._callbacks) {
+            frame.setActivityCallbacks(this);
+        }
+        // Modules will take care of calling super.onCreate, do not call it here
+        this._callbacks.onCreate(this, savedInstanceState, superProto.onCreate);
+
+        // Add custom initialization logic here
+        console.log("HORRAY, EXTERNAL ACTIVITY JS!");
+    },
+    onSaveInstanceState: function(outState) {
+        this._callbacks.onSaveInstanceState(this, outState, superProto.onSaveInstanceState);
+    },
+    onStart: function() {
+        this._callbacks.onStart(this, superProto.onStart);
+    },
+    onStop: function() {
+        this._callbacks.onStop(this, superProto.onStop);
+    },
+    onDestroy: function() {
+        this._callbacks.onDestroy(this, superProto.onDestroy);
+    },
+    onBackPressed: function() {
+        this._callbacks.onBackPressed(this, superProto.onBackPressed);
+    },
+    onRequestPermissionsResult: function (requestCode, permissions, grantResults) {
+        this._callbacks.onRequestPermissionsResult(this, requestCode, permissions, grantResults, undefined);
+    },
+    onActivityResult = function (requestCode, resultCode, data) {
+        this._callbacks.onActivityResult(this, requestCode, resultCode, data, _super.prototype.onActivityResult);
+    }
+});
