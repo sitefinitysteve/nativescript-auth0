2d319223c8dee31b9d9b2213f366214363ddab8b
Steve McNiven-Scott <steve@sitefinitysteve.com>
2017-05-26 10:50:01 -0400

    Update jwt code

--- jwt.js
+++ jwt.js
@@ -1,12 +1,11 @@
 'use strict';
 
-var base64_url_decode = require('./jwt-decode/base64_url_decode');
-var json_parse = require('./jwt-decode/json_parse');
+var jwt_decode = require('./jwt-decode/index');
 
 module.exports = function (token) {
   if (!token) {
     throw new Error('Invalid token specified');
   }
   
-  return json_parse(base64_url_decode(token.split('.')[1]));
+  return jwt_decode(token);
 };
