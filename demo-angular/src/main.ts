aa2b908d66b98dfe44239c32a71bdc586ea4e6cc
Pawel <pawel.losek@outlook.com>
2019-11-12 23:09:29 +0100

    Implemented angular demo

--- demo-angular/src/main.ts
+++ demo-angular/src/main.ts
@@ -2,5 +2,18 @@
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app/app.module";
+import { ios }  from '@nativescript/core/application';
+
+if (ios) {
+    const Auth0 = require("nativescript-auth0");
+    const delegate = require("./app/auth0/custom-app-delegate");
+
+    const CustomAppDelegate = delegate.CustomAppDelegate;
+    ios.delegate = CustomAppDelegate;
+
+    CustomAppDelegate.apply('applicationOpenURLOptions', (event) => {
+        return Auth0.resumeAuth(event.args.url, event.args.options);
+    });
+}
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
