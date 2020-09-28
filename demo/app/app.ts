f324fd7b73dcb923595173c65acba130398c7ddb
lochstar <lochlan@gmail.com>
2020-09-28 10:19:35 +1000

    update demo to ns7

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,15 +1,15 @@
-﻿import * as application from 'tns-core-modules/application';
+﻿import { Application } from '@nativescript/core';
 
-if (application.ios) {
+if (Application.ios) {
     const delegate = require('./custom-app-delegate');
     const Auth0 = require('nativescript-auth0');
 
     const CustomAppDelegate = delegate.CustomAppDelegate;
-    application.ios.delegate = CustomAppDelegate;
+    Application.ios.delegate = CustomAppDelegate;
 
     CustomAppDelegate.apply('applicationOpenURLOptions', (event) => {
         return Auth0.resumeAuth(event.args.url, event.args.options);
     });
 }
 
-application.run({ moduleName: 'main-page' });
+Application.run({ moduleName: 'main-page' });
