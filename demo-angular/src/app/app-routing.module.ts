3b3ebccf7362f11b3f22a764f8acec9032dbf1fa
Nathan Walker <walkerrunpdx@gmail.com>
2020-10-20 15:25:07 -0700

    chore: update angular demo

--- demo-angular/src/app/app-routing.module.ts
+++ demo-angular/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { NativeScriptRouterModule } from "@nativescript/angular";
 
 const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
-    { path: "home", loadChildren: "~/app/home/home.module#HomeModule" }
+    { path: "home", loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
 ];
 
 @NgModule({
